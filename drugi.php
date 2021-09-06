46c6903370b632f3bbe4a8640b8fb240a6560f5a
Kacper Domański <KacDom@Pracownia.com>
2021-09-06 13:12:25 +0200

    06092021

---
+++ drugi.php
@@ -0,0 +1,13 @@
+<?php
+
+$tablica = array("owca", "kot","pies","świnia","koń","żyrafa","żółw","papuga","królik","zając");
+
+
+for ($i = 0; $i < sizeof($tablica); $i++) {
+    echo($tablica[$i]);
+    echo('<br>');
+    }
+   
+ 
+    
+?>
